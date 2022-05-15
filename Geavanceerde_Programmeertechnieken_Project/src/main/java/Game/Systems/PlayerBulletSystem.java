cec487b17fcfcbaefe1cde647c43618fd118a4f0
Arafa42 <s107661@ap.be>
2022-05-15 17:36:33 +0200

    bullet bug fixed

--- Geavanceerde_Programmeertechnieken_Project/src/main/java/Game/Systems/PlayerBulletSystem.java
+++ Geavanceerde_Programmeertechnieken_Project/src/main/java/Game/Systems/PlayerBulletSystem.java
@@ -20,7 +20,7 @@ public class PlayerBulletSystem {
         //System.out.println(bullets.size());
         for(int i=0;i<bullets.size();i++) {
                 bullets.get(i).GetBulletComponent().setX(bullets.get(i).GetBulletComponent().getX() + (bullets.get(i).GetBulletComponent().getDx() * bullets.get(i).GetBulletComponent().getSpeed()));
-                if (bullets.get(i).GetBulletComponent().getX() < -bullets.get(i).GetBulletComponent().getR() || bullets.get(i).GetBulletComponent().getX() - 550 > bullets.get(i).GetBulletComponent().getScreenWidth() + bullets.get(i).GetBulletComponent().getR() || bullets.get(i).GetBulletComponent().getY() < -bullets.get(i).GetBulletComponent().getR() || bullets.get(i).GetBulletComponent().getY() > bullets.get(i).GetBulletComponent().getScreenHeight() + bullets.get(i).GetBulletComponent().getR()) {
+                if (bullets.get(i).GetBulletComponent().getX() < -bullets.get(i).GetBulletComponent().getR() || bullets.get(i).GetBulletComponent().getX() - 1000 > bullets.get(i).GetBulletComponent().getScreenWidth() + bullets.get(i).GetBulletComponent().getR() || bullets.get(i).GetBulletComponent().getY() < -bullets.get(i).GetBulletComponent().getR() || bullets.get(i).GetBulletComponent().getY() > bullets.get(i).GetBulletComponent().getScreenHeight() + bullets.get(i).GetBulletComponent().getR()) {
                     bullets.get(i).GetBulletComponent().setActive(false);
                     bullets.remove(i);
                 }
